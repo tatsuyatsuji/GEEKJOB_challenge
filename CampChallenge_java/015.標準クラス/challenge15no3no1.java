598fc759905106bfa51339f2f51084d48dfa06a9
辻竜也 <kimaguremomiji@gmail.com>
2017-07-19 11:20:22 +0900

    課題１５の３の１提出いたします

---
+++ CampChallenge_java/015.標準クラス/challenge15no3no1.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author tsujitatsuya
+ * ファイルに自己紹介を書き出し、保存してください。
+ */
+public class challenge15no3no1 extends HttpServlet {
+
+    
+    File fp = new File("test.txt");
+    
+    FileWriter fw;
+
+//    FileReader fr = new FileReader(fp);
+//    
+//    BufferedReader br = new BufferedReader(fr);
+//    
+//    
+//    public void kaki(PrintWriter pw) throws IOException{
+//        pw.print(br.readLine());
+//        
+//        fw.close();
+//        
+//    }
+//    
+    
+    
+    
+    public challenge15no3no1() throws IOException {
+        this.fw = new FileWriter(fp);
+        
+        fw.write("こんにちは辻と申します"
+                + "趣味はゲームやDTMです");
+        
+        fw.close();
+        
+    }
+    
+    
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet challenge15no3no1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet challenge15no3no1 at " + request.getContextPath() + "</h1>");
+//            kaki(out);
+//            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
